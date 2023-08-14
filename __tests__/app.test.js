ae46971f52ebe1857b527b95e8001f5fcf823e66
Lex Smith <lex.smith@live.com>
2023-08-14 15:14:57 +0100

    task 3 completed, test not dynamic

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -29,4 +29,24 @@ describe("GET requests", () => {
         });
       });
   });
+  test("200: returns api documentaion", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then((response) => {
+        // I think this should work but can't work out why it doesnt, please point me to the right notes to fix it.  
+        //for (const key in response.body) {
+        //   expect(key).toHaveProperty(['description']);
+        // }
+        expect(response.body["GET /api"].description).toEqual(
+          "serves up a json representation of all the available endpoints of the api"
+        );
+        expect(response.body["GET /api/topics"].description).toEqual(
+          "serves an array of all topics"
+        );
+        expect(response.body["GET /api/articles"].description).toEqual(
+          "serves an array of all articles"
+        );
+      });
+  });
 });
