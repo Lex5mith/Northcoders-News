5ec6350d8f2e09e9284eeae7656eb4a9065dcf27
Lex Smith <lex.smith@live.com>
2023-08-15 15:05:32 +0100

    completed task 5....I think...

--- controllers/article-controller.js
+++ controllers/article-controller.js
@@ -1,3 +1,5 @@
+const { request, response } = require("express");
+const { allArticlesWithCommentCount } = require("../models/article-model");
 const { fetchArticleById } = require("../models/article-model");
 
 const getArticleById = (request, response, next) => {
@@ -14,4 +16,15 @@ const getArticleById = (request, response, next) => {
     });
 };
 
-module.exports = { getArticleById };
+const getAllArticles = (request, response, next) => {
+  allArticlesWithCommentCount()
+    .then((articles) => {
+      // console.log("controller", articles);
+      return response.status(200).send({ articles });
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
+
+module.exports = { getArticleById, getAllArticles };
