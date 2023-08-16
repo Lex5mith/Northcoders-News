5cb66174760831d7f2a4f360f3768f63a0a36589
Lex Smith <lex.smith@live.com>
2023-08-16 14:12:35 +0100

    removed couple of cheeky console logs

--- models/users-model.js
+++ models/users-model.js
@@ -4,7 +4,6 @@ exports.allUserData = () => {
     let queryAllUserInfo = `SELECT username, name, avatar_url
     FROM users; `
     return db.query(queryAllUserInfo).then(({rows}) => {
-        console.log(rows)
         return rows
     })
 }
