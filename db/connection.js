66622f7e2bd98b056218bdd7b85f1ec205a9b978
Lex Smith <lex.smith@live.com>
2023-08-17 16:31:48 +0100

    connection running locally

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,22 @@
+const app = require("../app")
+
+
 const { Pool } = require('pg');
+
 const ENV = process.env.NODE_ENV || 'development';
+if (ENV === "production" && (!process.env.PGDATABASE && !process.env.DATABASE_URL)) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
+}
+
+const config = {}
+if(ENV === "production"){
+  config.connectionString = process.env.DATABASE_URL
+  config.max = 2
+}
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
-}
 
-module.exports = new Pool();
+module.exports = new Pool(config);
